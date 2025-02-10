d190aab33f6d7691729f5a44e36e93c000150349
Gap <puri.pongsomboonsuk@gmail.com>
2025-02-10 22:04:23 +0700

    Task09.5 endpoint for books

---
+++ src/server.ts
@@ -0,0 +1,60 @@
+import express, { Request, Response } from 'express'
+const app = express()
+const port = 3000
+
+interface Book {
+    title: string,
+    Author_name: string,
+    description: string,
+    groups: string
+}
+
+const books: Book[] = [
+    {
+        title: 'The Alchemist',
+        Author_name: 'Paulo Coelho',
+        description: 'The Alchemist is a novel by Brazilian author Paulo Coelho that was first published in 1988. Originally written in Portuguese, it became a widely translated international bestseller.',
+        groups: 'Fiction'
+    },
+    {
+        title: 'The Lean Startup',
+        Author_name: 'Eric Ries',
+        description: 'The Lean Startup: How Today’s Entrepreneurs Use Continuous Innovation to Create Radically Successful Businesses is a book by Eric Ries describing his proposed lean startup strategy for startup companies.',
+        groups: 'Business'
+    },
+    {
+        title: 'The 4-Hour Workweek',
+        Author_name: 'Tim Ferriss',
+        description: 'The 4-Hour Workweek: Escape 9-5, Live Anywhere, and Join the New Rich is a self-help book by Timothy Ferriss, an American writer, educational activist, and entrepreneur.',
+        groups: 'Business'
+    },
+    {
+        title: 'The Da Vinci Code',
+        Author_name: 'Dan Brown',
+        description: 'The Da Vinci Code is a 2003 mystery thriller novel by Dan Brown. It follows symbologist Robert Langdon and cryptologist Sophie Neveu after },
+        groups: 'Fiction'
+    },
+    {
+        title: 'The Great Gatsby',
+        Author_name: 'F. Scott Fitzgerald',
+        description: 'The Great Gatsby is a 1925 novel by American writer F. Scott Fitzgerald. Set in the Jazz Age on Long Island, near New York City, the novel depicts ',
+        groups: 'Fiction'
+    },
+    {   
+        title: 'The Catcher in the Rye',
+        Author_name: 'J.D. Salinger',
+        description: 'The Catcher in the Rye is a novel by J. D. Salinger, partially published in serial form in 1945–1946 and as a novel in 1951. It was originally intended ',
+        groups: 'Fiction'
+    }
+]
+
+app.get('/books', (req: Request, res: Response) => {
+    res.json(books)
+})
+
+app.listen(port, () => {
+    console.log(`App listening at http://localhost:${port}`)
+  })
+
+
+  
