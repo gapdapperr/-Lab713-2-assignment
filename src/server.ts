af837aa84704e737bb2675fbb338063b88dfd14a
Gap <puri.pongsomboonsuk@gmail.com>
2025-02-17 23:34:52 +0700

    Lab03 Task8-9

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,33 @@
 import express, { Request, Response } from 'express'
 import {getAllBooks, getBookByTitle, getBookById, addBook} from './services/bookService'
-
+import multer from 'multer'
+import {uploadFile} from './services/uploadFileService'
 import type {Book} from './models/book'
 const app = express()
 const port = 5000
 
 app.use(express.json())
 
+
+const upload = multer({ storage: multer.memoryStorage()})
+
+app.post('/upload', upload.single('file'), async (req: any, res: any) => {
+    try {
+       const file = req.file
+       if (!file) {
+        return res.status(400).send('No file uploaded.')
+       }
+
+       const bucket = 'images';
+       const filePath = `uploads/${file.originalname}`
+
+       await uploadFile(bucket, filePath, file)
+
+       res.status(200).send('File uploaded successfully.')
+    } catch (error) {
+        res.status(500).send('Error uploading file.')
+    }
+})
 
 app.get('/books', async (req: Request, res: Response) => {
     if (req.query.title) {
