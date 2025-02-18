5f1ab7111373f52c2badf9805dd3059c5340e7f4
gapdapper <puri.pongsomboonsuk@gmail.com>
2025-02-18 15:48:48 +0700

    Lab03 Task10

--- src/services/uploadFileService.ts
+++ src/services/uploadFileService.ts
@@ -1,7 +1,7 @@
 import S3Client from "../awsConfig";
 import { PutObjectCommand } from "@aws-sdk/client-s3";
 
-export async function uploadFile(bucket: string, filePath: string, file: Express.Multer.File): Promise<void> {
+export async function uploadFile(bucket: string, filePath: string, file: Express.Multer.File): Promise<string> {
     const params = {
         Bucket: bucket,
         Key: filePath,
@@ -12,6 +12,8 @@ export async function uploadFile(bucket: string, filePath: string, file: Express
     try {
         const data = await S3Client.send(new PutObjectCommand(params))
         console.log('File uploaded successfully', data)
+        const publicUrl = `https://bwmcfuxuhqmvcqblgilv.supabase.co/storage/v1/object/public/images/${filePath}`
+        return publicUrl
     }
     catch (error) {
         console.error('Error uploading file:', error)
