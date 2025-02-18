be3aafd87c38242d515b7e24290c8188421ca1d6
gapdapper <puri.pongsomboonsuk@gmail.com>
2025-02-18 15:59:46 +0700

    Lab03 Task11

--- src/services/uploadFileService.ts
+++ src/services/uploadFileService.ts
@@ -1,10 +1,23 @@
 import S3Client from "../awsConfig";
 import { PutObjectCommand } from "@aws-sdk/client-s3";
+import { exec } from "child_process";
+import {randomBytes} from 'crypto'
+
+
+function generateSaltedFilename(originalname: string): string {
+    const salt = randomBytes(16).toString('hex');
+    const extension = originalname.split('.').pop()
+    return `${salt}.${extension}`
+}
+
+
 
 export async function uploadFile(bucket: string, filePath: string, file: Express.Multer.File): Promise<string> {
+    const saltedFilename = generateSaltedFilename(file.originalname)
+    const saltedFilePath = `${filePath}/${saltedFilename}`
     const params = {
         Bucket: bucket,
-        Key: filePath,
+        Key: saltedFilePath,
         Body: file.buffer,
         ContentType: file.mimetype
     }
@@ -12,7 +25,7 @@ export async function uploadFile(bucket: string, filePath: string, file: Express
     try {
         const data = await S3Client.send(new PutObjectCommand(params))
         console.log('File uploaded successfully', data)
-        const publicUrl = `https://bwmcfuxuhqmvcqblgilv.supabase.co/storage/v1/object/public/images/${filePath}`
+        const publicUrl = `https://bwmcfuxuhqmvcqblgilv.supabase.co/storage/v1/object/public/images/${saltedFilePath}`
         return publicUrl
     }
     catch (error) {
